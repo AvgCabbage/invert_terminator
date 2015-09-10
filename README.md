f8c1ca3a3e7833822775f0d2aa856e003b26960a
Alex <a.oconnor222@gmail.com>
2015-09-10 15:03:56 -0400

    Remade with sed
    Rather than using multiple config files, this script will now just swap
    the values, using sed, in the one config file

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # invert_terminator
 Relaunch terminator with alternate config
 
-Something I wrote to swap between two terminator configs, one with white on black, the other black on white.  I found the two better for visibility reasons in different light on a dim laptop display, and depending on the colors behind the window (I like a little transparency in my term background).
+Something I wrote to swap between two terminator configs, one with white on black, the other black on white.  I found the two can be better for visibility reasons in different light on a dim laptop display, and depending on the colors behind the window (I like a little transparency in my term background).
+
+Settings up and alias to run this script and exit will switch the config and open a new term. (basically relaunch the term with the alternate settings)
 
 Just compares and swaps out config file, simple solution to the problem.  Plan is to completely rewrite this to just modify the foreground/background color values in the config, rather than have the extra config files.  V2 is partially working, just haven't gotten around to cleaning it up and finishing.
+
+Version 2 is now uploaded, which now uses sed to replace the foreground/background values in the config.
--- invert_term.sh
+++ invert_term.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-#Get MD5 of each file. Removes file name to just store the MD5 value
-CONF=$(md5sum ~/.config/terminator/config | sed -e 's/\ .\/home\/alex\/.config\/terminator\/config.*//');
-LIGHT=$(md5sum ~/.config/terminator/config.light | sed -e 's/\ .\/home\/alex\/.config\/terminator\/config.*//');
-DARK=$(md5sum ~/.config/terminator/config.dark | sed -e 's/\ .\/home\/alex\/.config\/terminator\/config.*//');
+#Script used to change terminator colors.
 
-	echo \"$CONF\" config
-	echo \"$LIGHT\" config.light
-	echo \"$DARK\" config.dark
-	echo
+CONFIG=~/.config/terminator/config
 
-	if [ "$CONF" == "$LIGHT" ] ; then
-	   cp /home/alex/.config/terminator/config.dark /home/alex/.config/terminator/config
-	   echo "Dark profile"
-	else
-	   cp /home/alex/.config/terminator/config.light /home/alex/.config/terminator/config
-	   echo "Light profile"
+DARK_FG="\"#ffffff\""
+DARK_BG="\"#333333\""
+
+LIGHT_FG="\"#000000\""
+LIGHT_BG="\"#ffffff\""
+
+CURRENT=$(grep -e "foreground_color\ =\ " $CONFIG)
+
+
+if [[ $CURRENT == "    foreground_color = $LIGHT_FG" ]]; then
+	sed -i.bak "s/foreground_color\ =\ $LIGHT_FG/foreground_color\ =\ $DARK_FG/g" $CONFIG
+	sed -i.bak "s/background_color\ =\ $LIGHT_BG/background_color\ =\ $DARK_BG/g" $CONFIG
+else
+	if [[ $CURRENT == "    foreground_color = $DARK_FG" ]]; then
+		sed -i.bak "s/foreground_color\ =\ $DARK_FG/foreground_color\ =\ $LIGHT_FG/g" $CONFIG
+		sed -i.bak "s/background_color\ =\ $DARK_BG/background_color\ =\ $LIGHT_BG/g" $CONFIG
 	fi
+fi
+
 
 terminator &
 
